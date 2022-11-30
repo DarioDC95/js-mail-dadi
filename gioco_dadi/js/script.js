3ff7f73a2b53bfdac8593616cfdc6b0f4c215e84
DarioDC95 <dario_95@live.com>
2022-11-30 16:42:34 +0100

    Exercise dices Done

---
+++ gioco_dadi/js/script.js
@@ -0,0 +1,21 @@
+// 1- Generare due numeri random in due variabili diverse
+
+// 1.1- una variabile per il giocatore
+let computer = Math.floor((Math.random() * 6) + 1);
+console.log(computer);
+
+// 1.2- una per il computer
+let player = Math.floor((Math.random() * 6) + 1);
+console.log(player);
+
+// 2- Stampare il giocatore con il numero più alto
+
+if (computer > player) {
+    console.log("computer wins");
+}
+else if (computer == player) {
+    console.log("draw");
+}
+else {
+    console.log("player wins");
+}
---
+++ gioco_dadi/readme.txt
@@ -0,0 +1,8 @@
+PROBLEMA: Gioco dei dadi 
+
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+1- Generare due numeri random in due variabili diverse
+    1.1- una variabile per il giocatore
+    1.2- una per il computer
+2- Stampare il giocatore con il numero più alto
